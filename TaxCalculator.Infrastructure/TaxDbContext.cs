572b1d9c44eb0fcab1b11e80cb04d09c60c80312
Andy <andymg@gmail.com>
2022-12-06 09:34:09 +0000

    Added TaxBand table seed data to migrations

--- TaxCalculator.Infrastructure/TaxDbContext.cs
+++ TaxCalculator.Infrastructure/TaxDbContext.cs
@@ -1,4 +1,5 @@
 using Microsoft.EntityFrameworkCore;
+using TaxCalculator.Domain.Models;
 using TaxCalculator.Infrastructure.Entities;
 
 namespace TaxCalculator.Infrastructure;
@@ -11,4 +12,35 @@ public class TaxDbContext : DbContext
     }
     
     public DbSet<TaxBandEntity> TaxBands { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<TaxBandEntity>()
+                    .HasData(new TaxBandEntity
+                             {
+                                 Id = 1,
+                                 Name = "Tax Band A",
+                                 LowerLimit = 0,
+                                 UpperLimit = 5000,
+                                 Rate = 0
+                             },
+                             new TaxBandEntity
+                             {
+                                 Id = 2,
+                                 Name = "Tax Band B",
+                                 LowerLimit = 5000,
+                                 UpperLimit = 20000,
+                                 Rate = 20
+                             },
+                             new TaxBandEntity
+                             {
+                                 Id = 3,
+                                 Name = "Tax Band C",
+                                 LowerLimit = 20000,
+                                 UpperLimit = -1,
+                                 Rate = 40
+                             });
+        
+        base.OnModelCreating(modelBuilder);
+    }
 }
