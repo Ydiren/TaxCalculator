eedab96fd285d7c6999f8b1d7120805f6f6e65d1
Andy <andymg@gmail.com>
2022-12-06 13:27:38 +0000

    Updated run.sh to include most steps required to build and run

--- run.sh
+++ run.sh
@@ -1,6 +1,9 @@
+dotnet publish -c release --no-self-contained ./TaxCalculator.sln
 
 docker run --name tax-calculator-db --rm -p 3306:3306 -e MYSQL_RANDOM_ROOT_PASSWORD=true -e MYSQL_DATABASE=tax-calculation -e MYSQL_USER=testuser -e MYSQL_PASSWORD=test1234 -d mysql:latest
 
-dotnet ef migrations add InitialCreate -c TaxDbContext -p ./TaxCalculator.Infrastructure/TaxCalculator.Infrastructure.csproj -s ../TaxCalculator.Api/TaxCalculator.Api.csproj
+dotnet ef database update -p ./TaxCalculator.Infrastructure/TaxCalculator.Infrastructure.csproj -s ./TaxCalculator.Api/TaxCalculator.Api.csproj 
 
-dotnet ef database update -p TaxCalculator.Infrastructure.csproj -s ../TaxCalculator.Api/TaxCalculator.Api.csproj 
+cd ./TaxCalculator.Api/bin/Release/net6.0/publish
+
+dotnet TaxCalculator.Api.dll
